7f357ec0a0d9cafef2566b2600265c063b97deeb
Rahul Chaudhary <rahulchaudhary99r@gmail.com>
2021-07-09 18:16:07 +0530

    UI Fix

--- src/utils/components/modal.js
+++ src/utils/components/modal.js
@@ -13,25 +13,27 @@ export default function StatusModal(props) {
                 closeOnBackdrop={false}
                 backdrop
                 centered
+                style={{fontFamily: "Poppins"}}
                 //color={props.status == 1 ? "danger" : "success" }
-                color="warning"
+                //color="warning"
                 
             >
                 <CModalHeader
-                    style={{height:"3rem"}}
+                   style={{ height: "3rem", backgroundColor: "teal", color: "white" }}
+                    
                     closeButton
-                ><CModalTitle>{props.status==1?`Block ${props.info}`:`Unblock ${props.info}`}</CModalTitle></CModalHeader>
+                ><CModalTitle>{props.status==1?`Block ${props.info}`:`Unblock ${props.info}`}?</CModalTitle></CModalHeader>
                 <CModalBody>
                     Are you sure you want to {props.status == 1 ? "block" : "unblock"} this { props.info}?
           </CModalBody>
                 <CModalFooter>
                     <CButton
-                        color="success"
-                        //style={{backgroundColor:"#008080",color:"#fff"}}
+                        //color="success"
+                        style={{backgroundColor:"#008080",color:"#fff"}}
                         onClick={()=>props.toggleStatus(props.toggleData)}
                     >Yes</CButton>
                     <CButton
-                        color="secondary"
+                        color="danger"
                         onClick={props.toggleModal}
                     >Cancel</CButton>
                 </CModalFooter>
